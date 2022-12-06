ed6616f00e04b8b0045655e82c523db3f1520ae0
Joseph.Hess@Stonybrook.edu <Joseph.Hess@Stonybrook.edu>
2022-12-05 20:08:06 -0500

    Bottom of the screen now tells you what you are searching by

--- client/src/components/CreateNewListBar.js
+++ client/src/components/CreateNewListBar.js
@@ -1,8 +1,7 @@
-import { Button } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useContext } from "react";
 import GlobalStoreContext from "../store";
 import PlaylistStoreContext from "../store/PlaylistStore";
-
 
 export default function CreateNewListBar(props) {
     const { store } = useContext(PlaylistStoreContext)
@@ -11,9 +10,35 @@ export default function CreateNewListBar(props) {
         store.createPlaylist();
     }
 
+    let bottomMessage = "Viewing your own lists"
+
+    if(store.searchMode === "BY_USER") {
+        if(store.searchValue === "") {
+            bottomMessage = "Enter a search term to search by username"
+        } else {
+            bottomMessage = "Viewing playlists by users with " + store.searchValue + " in their name"
+        }
+    } else if(store.searchMode === "BY_NAME") {
+        if(store.searchValue === "") {
+            bottomMessage = "Enter a search term to search by playlist name"
+        } else {
+            bottomMessage = "Viewing playlists with " + store.searchValue + " in their title"
+        }
+    }
+
     return (
-        <Button onClick={handleCreateNewList}>
-            Add List
-        </Button>
+        <Grid container flexDirection={"row"}>
+            <Grid item xs={6} container justifyContent={"flex-end"}>
+                <Button onClick={handleCreateNewList}>
+                    Add List
+                </Button>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography>
+                    { bottomMessage }
+                </Typography>
+            </Grid>
+        </Grid>
+        
     )
 }
